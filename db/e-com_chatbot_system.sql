83eb48e6beeff56048f923b2e0f93fe66fe43279
Muhammad Usman Umer <62427775+usmanumer1106@users.noreply.github.com>
2021-01-17 01:15:04 +0500

    Update e-com_chatbot_system.sql

--- db/e-com_chatbot_system.sql
+++ db/e-com_chatbot_system.sql
@@ -3,11 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 21, 2020 at 07:22 PM
+-- Generation Time: Jan 16, 2021 at 08:55 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.9
---------------------------------------
---------------------------------------
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -45,7 +43,7 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`admin_id`, `admin_name`, `admin_email`, `admin_password`, `admin_phone`, `admin_type`, `admin_address`, `admin_status`) VALUES
-(1, 'Muhammad Usman Umer', 'usmanumer1106@gmail.com', 'admin', '03208500106', 'su', 'House No 4 Main Bazaar Raza Town Sargodha', 1);
+(3, 'Muhammad Usman Umer', 'usmanumer106@gmail.com', '$2y$10$loyv/6VmdwQNTu8hR8hV3.3s9LsJiYgxTSjWczLC1F9bWRq1iQOuu', '03208500106', 'su', 'House #4 Main Bazaar Raza Town', 1);
 
 -- --------------------------------------------------------
 
@@ -56,29 +54,6 @@ INSERT INTO `admin` (`admin_id`, `admin_name`, `admin_email`, `admin_password`, 
 CREATE TABLE `category` (
   `category_id` int(255) NOT NULL,
   `category_name` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `city`
---
-
-CREATE TABLE `city` (
-  `city_id` int(255) NOT NULL,
-  `city_name` varchar(255) NOT NULL,
-  `province_id` int(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `company`
---
-
-CREATE TABLE `company` (
-  `company_id` int(255) NOT NULL,
-  `company_name` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -118,12 +93,34 @@ CREATE TABLE `product` (
   `product_name` varchar(255) NOT NULL,
   `product_price` int(255) NOT NULL,
   `product_description` varchar(255) NOT NULL,
-  `product_quantity` int(255) NOT NULL,
   `product_discount` int(255) NOT NULL,
   `product_status` int(255) NOT NULL,
-  `product_size` varchar(255) NOT NULL,
-  `category_id` int(255) NOT NULL,
-  `company_id` int(255) NOT NULL
+  `category_id` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `size`
+--
+
+CREATE TABLE `size` (
+  `size_id` int(255) NOT NULL,
+  `size_name` varchar(255) NOT NULL,
+  `size_quantity` int(255) NOT NULL,
+  `product_id` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subcategory`
+--
+
+CREATE TABLE `subcategory` (
+  `subcategory_id` int(255) NOT NULL,
+  `subcategory_name` varchar(255) NOT NULL,
+  `category_id` int(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -140,8 +137,7 @@ CREATE TABLE `user` (
   `user_address` varchar(255) NOT NULL,
   `user_status` int(255) NOT NULL DEFAULT 1,
   `user_password` varchar(255) NOT NULL,
-  `user_phone` varchar(255) NOT NULL,
-  `city_id` int(255) NOT NULL
+  `user_phone` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -159,18 +155,6 @@ ALTER TABLE `admin`
 --
 ALTER TABLE `category`
   ADD PRIMARY KEY (`category_id`);
-
---
--- Indexes for table `city`
---
-ALTER TABLE `city`
-  ADD PRIMARY KEY (`city_id`);
-
---
--- Indexes for table `company`
---
-ALTER TABLE `company`
-  ADD PRIMARY KEY (`company_id`);
 
 --
 -- Indexes for table `order`
@@ -192,15 +176,27 @@ ALTER TABLE `order_detail`
 --
 ALTER TABLE `product`
   ADD PRIMARY KEY (`product_id`),
-  ADD KEY `category_id` (`category_id`),
-  ADD KEY `company_id` (`company_id`);
+  ADD KEY `category_id` (`category_id`);
+
+--
+-- Indexes for table `size`
+--
+ALTER TABLE `size`
+  ADD PRIMARY KEY (`size_id`),
+  ADD KEY `product_id` (`product_id`);
+
+--
+-- Indexes for table `subcategory`
+--
+ALTER TABLE `subcategory`
+  ADD PRIMARY KEY (`subcategory_id`),
+  ADD KEY `category_id` (`category_id`);
 
 --
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`user_id`),
-  ADD KEY `city_id` (`city_id`);
+  ADD PRIMARY KEY (`user_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -210,25 +206,13 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `admin_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `admin_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `category`
 --
 ALTER TABLE `category`
   MODIFY `category_id` int(255) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `city`
---
-ALTER TABLE `city`
-  MODIFY `city_id` int(255) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `company`
---
-ALTER TABLE `company`
-  MODIFY `company_id` int(255) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `order`
@@ -247,6 +231,18 @@ ALTER TABLE `order_detail`
 --
 ALTER TABLE `product`
   MODIFY `product_id` int(255) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `size`
+--
+ALTER TABLE `size`
+  MODIFY `size_id` int(255) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `subcategory`
+--
+ALTER TABLE `subcategory`
+  MODIFY `subcategory_id` int(255) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `user`
@@ -275,14 +271,19 @@ ALTER TABLE `order_detail`
 -- Constraints for table `product`
 --
 ALTER TABLE `product`
-  ADD CONSTRAINT `product_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `category` (`category_id`),
-  ADD CONSTRAINT `product_ibfk_2` FOREIGN KEY (`company_id`) REFERENCES `company` (`company_id`);
+  ADD CONSTRAINT `product_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `category` (`category_id`);
 
 --
--- Constraints for table `user`
+-- Constraints for table `size`
 --
-ALTER TABLE `user`
-  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`city_id`) REFERENCES `city` (`city_id`);
+ALTER TABLE `size`
+  ADD CONSTRAINT `size_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `product` (`product_id`);
+
+--
+-- Constraints for table `subcategory`
+--
+ALTER TABLE `subcategory`
+  ADD CONSTRAINT `subcategory_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `category` (`category_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
